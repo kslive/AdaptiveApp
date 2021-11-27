81cea79f28a815998147a310172363fc4797c34e
Eugene <Kiselev>
2021-11-27 13:11:41 +0300

    Initial Commit

---
+++ AdaptiveApp/AdaptiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptiveAppApp.swift
+//  AdaptiveApp
+//
+//  Created by Eugene Kiselev on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AdaptiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
